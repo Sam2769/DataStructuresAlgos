5c5e21071f1b722684cf51c0d2964ba1aa632dd8
Saksham Verma <sakshamverma1000@gmail.com>
2023-11-14 15:09:21 +0530

    DSA.py is stored

---
+++ DSA.py
@@ -0,0 +1,23 @@
+class btNode:
+
+    def __init__(self,data):
+
+        self.left = None
+        self.right = None
+        self.value = data
+
+
+class BinaryTree:
+
+    def __init__(self,root):
+
+        self.root = root
+
+
+
+
+
+
+
+
+
